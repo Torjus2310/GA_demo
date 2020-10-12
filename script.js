f92a78dd420684e279a48da05f850e8c360878b6
Torjus2310 <56019539+Torjus2310@users.noreply.github.com>
2020-10-12 20:39:24 +0200

    added button

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let product = document.querySelector('.product')
 
+let frame = 0;
 
 
 window.addEventListener ('wheel', (event) =>{
 
-    event.deltaY
-    product.style.cssText = "background-position-x: calc(1 * -730px)"    
+    //event.deltaY
+    product.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -730px);";
+    frame += 0.1;
+    if(frame > 4) frame = 0;
     
 })
